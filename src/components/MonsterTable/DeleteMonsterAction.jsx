622bfae2d59ab3ae8ce903919a5971fd68c0f233
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-12-18 22:43:50 +0100

    add toast message for delete monster

--- src/components/MonsterTable/DeleteMonsterAction.jsx
+++ src/components/MonsterTable/DeleteMonsterAction.jsx
@@ -1,28 +1,28 @@
+import React from 'react';
 import { IconButton, useDisclosure } from '@chakra-ui/react';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { useContext } from 'react';
-import { MonsterContext } from '../../context/MonsterContext/MonsterContext'; // Asegúrate de importar correctamente el contexto
+import { MonsterContext } from '../../context/MonsterContext/MonsterContext';
+import DeleteMonsterModal from './DeleteMonsterModal';
 
 function DeleteMonsterAction({ monster }) {
-    console.log('Monster:', monster); // Agrega este log para depurar
     const { deleteMonsterById } = useContext(MonsterContext);
     const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const handleDelete = async (id) => {
-        console.log('Deleting monster with ID:', id); // Agrega este log para depurar
-        if (id) {
-            await deleteMonsterById(id);
-        } else {
-            console.error('Monster ID is undefined');
-        }
-    };
-
     return (
-        <IconButton
-            icon={<DeleteIcon />}
-            colorScheme="red"
-            onClick={() => handleDelete(monster?.id)}
-        />
+        <>
+            <IconButton
+                icon={<DeleteIcon />}
+                colorScheme="red"
+                onClick={onOpen}
+            />
+            <DeleteMonsterModal
+                isOpen={isOpen}
+                onClose={onClose}
+                monsterId={monster?.id}
+                deleteMonsterById={deleteMonsterById}
+            />
+        </>
     );
 }
 
